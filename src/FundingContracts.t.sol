b70b4fd9dfd3d8f34f5f5fe0c25f6b1e4be48ad7
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2021-09-01 14:28:40 +0200

    dapp init FundingContracts

---
+++ src/FundingContracts.t.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.4;
+
+import "ds-test/test.sol";
+
+import "./FundingContracts.sol";
+
+contract FundingContractsTest is DSTest {
+    FundingContracts contracts;
+
+    function setUp() public {
+        contracts = new FundingContracts();
+    }
+
+    function testFail_basic_sanity() public {
+        assertTrue(false);
+    }
+
+    function test_basic_sanity() public {
+        assertTrue(true);
+    }
+}
