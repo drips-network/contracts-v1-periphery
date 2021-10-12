86969fa868e6bdfe9a40864b51365a48f063aaa2
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2021-10-12 10:29:26 +0200

    initial metadata

---
+++ src/metadata.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.7;
+import "./base64.sol";
+
+contract MetaDataBuilder {
+    function buildMetaData(string memory projectName, uint tokenId, uint128 amtPerCycle, bool active) public pure returns (string memory) {
+        string memory supportRateString = uint2str(amtPerCycle/ 1 ether);
+        string memory tokenIdString = uint2str(tokenId);
+        string memory tokenActive = "false";
+        if (active) {
+            tokenActive = "true";
+        }
+        uint128 decimal = (amtPerCycle % 1 ether) / 10**16;
+        if (decimal > 0) {
+            supportRateString = string(abi.encodePacked(supportRateString, ".",uint2str(decimal)));
+        }
+
+        string memory svg = string(
+            abi.encodePacked(
+                '<svg class="svgBody" width="300" height="300" viewBox="0 0 300 300" fill="white" xmlns="http://www.w3.org/2000/svg">',
+                "<style>svg { background-color: black; }</style>",
+                '<text x="20" y="20" font-family="Courier New, Courier, Lucida Sans Typewriter" class="small"> \xf0\x9f\x8c\xb1 Radicle Funding \xf0\x9f\x8c\xb1 </text>',
+                '<text x="20" y="80" class="medium">Project Name:</text>  <text x="150" y="80" class="small">',
+                projectName,
+                "</text>",
+                '<text x="20" y="100" class="medium">NFT-ID:</text><text x="150" y="100" class="small">',
+                tokenIdString,
+                "</text>",
+                '<text x="20" y="120" class="medium">Support-Rate:</text><text x="150" y="120" class="small">',
+                supportRateString,
+                " DAI</text>",
+                "</svg>"
+            )
+        );
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"projectName":"',
+                                projectName,
+                                '", ',
+                                 '{"tokenId":"',
+                                tokenIdString,
+                                '", ',
+                                '{"active":"',
+                                tokenActive,
+                                '", ',
+                                '"image": "',
+                                "data:image/svg+xml;base64,",
+                                svg,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
+    }
+
+    function uint2str(uint256 _i)
+        internal
+        pure
+        returns (string memory _uintAsString)
+    {
+        if (_i == 0) {
+            return "0";
+        }
+        uint256 j = _i;
+        uint256 len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint256 k = len;
+        while (_i != 0) {
+            k = k - 1;
+            uint8 temp = (48 + uint8(_i - (_i / 10) * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
+    }
+
+}
--- src/nft.sol
+++ src/nft.sol
@@ -25,13 +25,19 @@ contract FundingNFT is ERC721, Ownable {
         uint128 minted;
     }
 
-    mapping (uint128 => NFTType) public nftTypes;
+    mapping(uint128 => NFTType) public nftTypes;
 
     // events
     event NewNFTType(uint128 indexed nftType, uint128 limit);
 
-    constructor(FundingPool pool_, string memory name_, string memory symbol_, address owner_,
-        uint128 minAmtPerSec_, InputNFTType[] memory inputNFTTypes) ERC721(name_, symbol_) {
+    constructor(
+        FundingPool pool_,
+        string memory name_,
+        string memory symbol_,
+        address owner_,
+        uint128 minAmtPerSec_,
+        InputNFTType[] memory inputNFTTypes
+    ) ERC721(name_, symbol_) {
         pool = FundingPool(pool_);
         dai = pool.erc20();
         minAmtPerSec = minAmtPerSec_;
@@ -40,11 +46,14 @@ contract FundingNFT is ERC721, Ownable {
     }
 
     function addTypes(InputNFTType[] memory inputNFTTypes) public onlyOwner {
-        for(uint i = 0; i < inputNFTTypes.length; i++) {
+        for (uint256 i = 0; i < inputNFTTypes.length; i++) {
             uint128 limit = inputNFTTypes[i].limit;
             uint128 nftTypeId = inputNFTTypes[i].nftTypeId;
             // nftType already exists or limit is not > 0
-            require(nftTypes[nftTypeId].limit == 0, "nftTypeId-already-in-usage");
+            require(
+                nftTypes[nftTypeId].limit == 0,
+                "nftTypeId-already-in-usage"
+            );
             require(limit > 0, "zero-limit-not-allowed");
 
             nftTypes[nftTypeId].limit = limit;
@@ -52,19 +61,39 @@ contract FundingNFT is ERC721, Ownable {
         }
     }
 
-    function createTokenId(uint128 id, uint128 nftType) public pure returns(uint tokenId) {
-        return uint((uint(nftType) << 128)) | id;
+    function addType(uint128 nftTypeId, uint128 limit) public onlyOwner {
+        require(nftTypes[nftTypeId].limit == 0, "nftTypeId-already-in-usage");
+        require(limit > 0, "zero-limit-not-allowed");
+
+        nftTypes[nftTypeId].limit = limit;
+        emit NewNFTType(nftTypeId, limit);
     }
 
-    function tokenType(uint tokenId) public pure returns(uint128 nftType) {
+    function createTokenId(uint128 id, uint128 nftType)
+        public
+        pure
+        returns (uint256 tokenId)
+    {
+        return uint256((uint256(nftType) << 128)) | id;
+    }
+
+    function tokenType(uint256 tokenId) public pure returns (uint128 nftType) {
         return uint128(tokenId >> 128);
     }
 
-    function mint(address nftReceiver, uint128 typeId, uint128 topUp, uint128 amtPerSec) external returns (uint256) {
+    function mint(
+        address nftReceiver,
+        uint128 typeId,
+        uint128 topUp,
+        uint128 amtPerSec
+    ) external returns (uint256) {
         require(amtPerSec >= minAmtPerSec, "amt-per-sec-too-low");
         uint128 cycleSecs = uint128(pool.cycleSecs());
         require(topUp >= amtPerSec * cycleSecs, "toUp-too-low");
-        require(nftTypes[typeId].minted++ < nftTypes[typeId].limit, "nft-type-reached-limit");
+        require(
+            nftTypes[typeId].minted++ < nftTypes[typeId].limit,
+            "nft-type-reached-limit"
+        );
 
         uint256 newTokenId = createTokenId(nftTypes[typeId].minted, typeId);
 
@@ -76,8 +105,15 @@ contract FundingNFT is ERC721, Ownable {
 
         // start streaming
         ReceiverWeight[] memory receivers = new ReceiverWeight[](1);
-        receivers[0] = ReceiverWeight({receiver: owner(), weight:1});
-        pool.updateSender(address(this), newTokenId, topUp, 0, amtPerSec, receivers);
+        receivers[0] = ReceiverWeight({receiver: owner(), weight: 1});
+        pool.updateSender(
+            address(this),
+            newTokenId,
+            topUp,
+            0,
+            amtPerSec,
+            receivers
+        );
 
         // transfer nft from contract to receiver
         _transfer(address(this), nftReceiver, newTokenId);
@@ -85,13 +121,17 @@ contract FundingNFT is ERC721, Ownable {
         return newTokenId;
     }
 
-    function withdrawable(uint128 tokenId) public view returns(uint128) {
+    function withdrawable(uint128 tokenId) public view returns (uint128) {
         return pool.maxWithdraw(pool.nftID(address(this), tokenId));
     }
 
-    function secsUntilInactive(uint128 tokenId) public view returns(uint128) {
-        uint128 withdrawable = pool.withdrawable(pool.nftID(address(this), tokenId));
-        uint128 amtPerSecond = pool.amtPerSecond(pool.nftID(address(this), tokenId));
+    function secsUntilInactive(uint128 tokenId) public view returns (uint128) {
+        uint128 withdrawable = pool.withdrawable(
+            pool.nftID(address(this), tokenId)
+        );
+        uint128 amtPerSecond = pool.amtPerSecond(
+            pool.nftID(address(this), tokenId)
+        );
 
         uint128 secsLeft = pool.currLeftSecsInCycle();
         // nft inactive: not enough funds for current cycle
@@ -106,7 +146,12 @@ contract FundingNFT is ERC721, Ownable {
     }
 
     // todo needs to be implemented
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory)  {
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
         // test metadata json
         return "";
     }
@@ -116,54 +161,5 @@ contract FundingNFT is ERC721, Ownable {
         // test project data json
         return "QmdFspZJyihiG4jESmXC72VfkqKKHCnNSZhPsamyWujXxt";
     }
-}
 
-contract SVG {
-        // todo needs to be implemented
-    function tokenURI(uint256 tokenId) public view returns (string memory)  {
-        // test metadata json
-        return generateImage(tokenId);
-    }
-
-    // todo needs to be implemented
-    function contractURI() public view returns (string memory) {
-        // test project data json
-        return "QmdFspZJyihiG4jESmXC72VfkqKKHCnNSZhPsamyWujXxt";
-    }
-    function generateImage(uint256 tokenId) public view returns (string memory) {
-        return string(
-            abi.encodePacked(
-                '<svg class="svgBody" width="300" height="300" viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg">',
-                '<text x="215" y="80" class="small">Radicle Funding</text>',
-                '<text x="15" y="80" class="medium">EDITION>1</text>',
-                '<text x="15" y="100" class="medium">ID> ',toString(tokenId),'</text>',
-                '</svg>'
-            )
-        );
-    }
-    // from: https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/utils/Strings.sol
-    /**
-     * @dev Converts a `uint256` to its ASCII `string` decimal representation.
-     */
-    function toString(uint256 value) internal pure returns (string memory) {
-        // Inspired by OraclizeAPI's implementation - MIT licence
-        // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
-
-        if (value == 0) {
-            return "0";
-        }
-        uint256 temp = value;
-        uint256 digits;
-        while (temp != 0) {
-            digits++;
-            temp /= 10;
-        }
-        bytes memory buffer = new bytes(digits);
-        while (value != 0) {
-            digits -= 1;
-            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
-            value /= 10;
-        }
-        return string(buffer);
-    }
 }
