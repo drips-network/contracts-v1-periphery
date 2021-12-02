534f61e5fd97757969acbe10dabc8392eb968cfb
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2021-12-02 23:44:50 +0100

    feat: updated deploy script with ipfs builder

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,10 +18,12 @@ addValuesToFile() {
 
 DEPLOYMENT_FILE=${1:-./deployment_$(seth chain).json}
 GOVERNANCE=${1:-0x$ETH_FROM}
+DEFAULT_IPFS_IMG=${1:-QmcjdWo3oDYPGdLCdmEpGGpFsFKbfXwCLc5kdTJj9seuLx}
 
 message Deployment Config
-echo "Governance Address: $GOVERNANCE"
-echo "DAI Address:        $DAI"
+echo "Governance Address:       $GOVERNANCE"
+echo "DAI Address:              $DAI"
+echo "Default IPFS Hash image:  $DEFAULT_IPFS_IMG"
 
 # build contracts
 message Build Contracts
@@ -32,10 +34,10 @@ dapp build
 [ -z "$CYCLE_SECS" ] && CYCLE_SECS=86400 # one day secs
 message Funding Contracts Deployment
 
-[ -z "$DRIPS_HUB" ] && DRIPS_HUB=$(dapp create DaiDripsHub $CYCLE_SECS $DAI)
+[ -z "$DRIPS_HUB" ] && DRIPS_HUB=$(dapp create DaiDripsHub $CYCLE_SECS $GOVERNANCE $DAI)
 echo "Drips Hub Contract: $DRIPS_HUB"
 
-[ -z "$BUILDER" ] && BUILDER=$(dapp create Builder)
+[ -z "$BUILDER" ] && BUILDER=$(dapp create DefaultIPFSBuilder $GOVERNANCE "\"$DEFAULT_IPFS_IMG\"")
 echo "Builder Contract: $BUILDER"
 
 [ -z "$RADICLE_REGISTRY" ] && RADICLE_REGISTRY=$(dapp create RadicleRegistry $DRIPS_HUB $BUILDER $GOVERNANCE)
@@ -46,13 +48,14 @@ touch $DEPLOYMENT_FILE
 addValuesToFile $DEPLOYMENT_FILE <<EOF
 {
     "CONTRACT_DAI"               : "$DAI",
-    "CONTRACT_DRIPS_HUB"      : "$DRIPS_HUB",
+    "CONTRACT_DRIPS_HUB"         : "$DRIPS_HUB",
     "CONTRACT_RADICLE_REGISTRY"  : "$RADICLE_REGISTRY",
     "CONTRACT_BUILDER"           : "$BUILDER",
     "NETWORK"                    : "$(seth chain)",
     "DEPLOY_ADDRESS"             : "$ETH_FROM",
     "CYCLE_SECS"                 : "$CYCLE_SECS",
-    "COMMIT_HASH"                :  "$(git --git-dir .git rev-parse HEAD )"
+    "COMMIT_HASH"                : "$(git --git-dir .git rev-parse HEAD )",
+    "GOVERNANCE_ADDRESS"         : "$GOVERNANCE"
 }
 EOF
 
@@ -62,11 +65,11 @@ cat $DEPLOYMENT_FILE
 
 message Verify Contracts on Etherscan
 if [ -n "$ETHERSCAN_API_KEY" ]; then
-  dapp verify-contract --async 'lib/radicle-drips-hub/src/DaiDripsHub.sol:DaiDripsHub' $DRIPS_HUB $CYCLE_SECS $DAI
+  dapp verify-contract --async 'lib/radicle-drips-hub/src/DaiDripsHub.sol:DaiDripsHub' $DRIPS_HUB $CYCLE_SECS $GOVERNANCE $DAI
   dapp verify-contract --async 'src/registry.sol:RadicleRegistry' $RADICLE_REGISTRY $DRIPS_HUB $BUILDER $GOVERNANCE
-  dapp verify-contract --async 'src/builder.sol:Builder' $BUILDER
+  dapp verify-contract --async 'src/ipfsBuilder.sol:DefaultIPFSBuilder' $BUILDER $GOVERNANCE "\"$DEFAULT_IPFS_IMG\""
   TOKEN_TEMPLATE=$(seth call $RADICLE_REGISTRY 'dripTokenTemplate()(address)')
   dapp verify-contract --async 'src/token.sol:DripsToken' $TOKEN_TEMPLATE $DRIPS_HUB
 else
-    echo "No ETHERSCAN_API_KEY for contract verification provided"
+  echo "No ETHERSCAN_API_KEY for contract verification provided"
 fi
