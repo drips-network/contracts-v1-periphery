7eb7659b89a9d528eec46683d74de910591d3ac4
Manuel Polzhofer <manuel.polzhofer@protonmail.com>
2022-04-06 17:36:57 +0200

    feat: added multihash event with id

--- src/metadata.sol
+++ src/metadata.sol
@@ -2,15 +2,24 @@
 pragma solidity ^0.8.7;
 
 contract MetaData {
-    event MultiHash(address indexed addr, bytes multiHash);
+    bytes32 public constant DEFAULT_ID = "identity";
+
+    // correct multiHash construction see https://github.com/multiformats/multihash
+    // 0-1  bytes:  hashFunction
+    // 1-2  bytes:  size
+    // 2-34 bytes:  hash (in most cases 32 bytes but not guranteed)
+    event MultiHash(bytes32 indexed id, address indexed addr, bytes multiHash);
 
     /// @notice publish an IPFS hash as an event
     /// @param multiHash as bytes array
     function publish(bytes calldata multiHash) external {
-        // correct multiHash construction see https://github.com/multiformats/multihash
-        // 0-1  bytes:  hashFunction
-        // 1-2  bytes:  size
-        // 2-34 bytes:  hash (in most cases 32 bytes but not guranteed)
-        emit MultiHash(msg.sender, multiHash);
+        emit MultiHash(DEFAULT_ID, msg.sender, multiHash);
+    }
+
+    /// @notice publish an IPFS hash as an event with an id
+    /// @param multiHash as bytes array
+    /// @param id identifier for the multiHash
+    function publish(bytes32 id, bytes calldata multiHash) external {
+        emit MultiHash(id, msg.sender, multiHash);
     }
 }
