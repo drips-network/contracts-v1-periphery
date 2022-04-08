b28175782307bcfdc6ba6f6238acf9b7133ed040
Manuel Polzhofer <manuel.polzhofer@protonmail.com>
2022-04-08 16:38:02 +0200

    fix: event order params

--- src/metadata.sol
+++ src/metadata.sol
@@ -2,24 +2,24 @@
 pragma solidity ^0.8.7;
 
 contract MetaData {
-    bytes32 public constant DEFAULT_ID = "identity";
+    bytes32 public constant DEFAULT_ID = "";
 
     // correct multiHash construction see https://github.com/multiformats/multihash
     // 0-1  bytes:  hashFunction
     // 1-2  bytes:  size
     // 2-34 bytes:  hash (in most cases 32 bytes but not guranteed)
-    event MultiHash(bytes32 indexed id, address indexed addr, bytes multiHash);
+    event MultiHash(address indexed addr, bytes32 indexed id, bytes multiHash);
 
     /// @notice publish an IPFS hash as an event
     /// @param multiHash as bytes array
     function publish(bytes calldata multiHash) external {
-        emit MultiHash(DEFAULT_ID, msg.sender, multiHash);
+        emit MultiHash(msg.sender, DEFAULT_ID, multiHash);
     }
 
     /// @notice publish an IPFS hash as an event with an id
     /// @param multiHash as bytes array
     /// @param id identifier for the multiHash
     function publish(bytes32 id, bytes calldata multiHash) external {
-        emit MultiHash(id, msg.sender, multiHash);
+        emit MultiHash(msg.sender, id, multiHash);
     }
 }
